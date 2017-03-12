c5428e99a906b9f4fe06d09835e224cf7ee2a796
ileonidmas <ileonidmas@gmail.com>
2017-03-12 16:38:18 +0100

    new changes with rows

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -24,7 +24,7 @@ public class Evaluator {
     {
         //preparation
 
-        double columnEvaluationValue = EvaluateColumns(board);
+        EvaluateColumns(board);
         EvaluateRows(board);
         return 0;
     }
@@ -46,7 +46,7 @@ public class Evaluator {
                                 containsOne = true;
                             else
                                 containsTwo = true;
-                            tempArray[index] = 1;
+                            tempArray[index] = board[x][y+index];
                         }
                         else
                             tempArray[index]=0;
@@ -62,54 +62,41 @@ public class Evaluator {
             }
     }
 
-    private double EvaluateColumns(Integer[][] board){
-        HashMap<Integer, Integer> maxValues = new HashMap();
-        maxValues.put(1, 0);
-        maxValues.put(2, 0);
-        maxValues.put(3, 0);
-        HashMap<Integer, Integer> minValues= new HashMap();;
-        minValues.put(1, 0);
-        minValues.put(2, 0);
-        minValues.put(3, 0);
+    private void EvaluateColumns(Integer[][] board){
+        boolean isPlayerOne = false;
+        boolean isPlayerTwo = false;
+        Integer[] tempArray = new Integer[]{0,0,0,0};
         int rowSize = board.length,columnSize  = board[0].length;
         if (rowSize > 3)
             for (int y = 0 ; y < columnSize ; y++) {
-                int counter = 1;
-                Integer foundValue = 0;
+                int counter = 0;
                 boolean valueSet = false;
-                if(board[0][y]==null && board[rowSize-1][y]!= null) { //check if first element of a columns is zero or lasat
+                if(board[0][y]==null && board[rowSize-1][y]!= null) { //check if first element of a columns is zero or last
                     for (int x = 0; x < rowSize; x++) {
                         if (board[x][y] != null) {
                             if (!valueSet)
                             {
-                                foundValue=board[x][y];
+                                tempArray[counter]=board[x][y];
                                 valueSet = true;
                             }
                             else
                             {
-                                counter++;
-                                if (board[x][y]!=foundValue){
-                                    counter--;
+                                if (board[x][y]!=tempArray[0]){
                                     break;
+                                }
+                                else
+                                {
+                                    counter++;
+                                    tempArray[counter] = board[x][y];
                                 }
                             }
                         }
                     }
-                    if(foundValue == maxPlayerId)
-                        maxValues.put(counter, maxValues.get(counter) + 1);
-                    else
-                        minValues.put(counter, minValues.get(counter) + 1);
                 }
+
+                evaluationScore.addCount(tempArray,tempArray[0]);
             }
-        //Evaluate rows
-        if (maxValues.get(3)>0) return 1;
-        if (minValues.get(3)>1) return -1;
-        double maxValue = maxValues.get(2) * 2 + maxValues.get(1) * 1;
-        double minValue = minValues.get(3) * 3 + minValues.get(2) * 2  + minValues.get(1);
-        if (maxValue>minValue)
-            return minValue/maxValue;
-        else
-            return -(maxValue/minValue);
+
     }
 
     /**
